a14773e74ebb85fa53dc32034f6b4668b85c4a49
Poveda <julianopoveda@bitbucket.org>
2018-07-22 21:48:28 -0300

    Inicio da implementação do indice IGP-M

--- CalculadorIndices/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace CalculadorIndices
-{
-    public class Class1
-    {
-    }
-}
---
+++ CalculadorIndices/IndicesCorrecao/IGPM.cs
@@ -0,0 +1,30 @@
+﻿using CalculadorIndices.Domain;
+using CalculadorIndices.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace CalculadorIndices.IndicesCorrecao
+{
+    public class IGPM : IIndiceCorrecao, IIndiceCorrecaoDiscriminado
+    {
+        public ValorCorrigido Corrigir(decimal valorInicial, IEnumerable<Indice> indices)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ValorCorrigido Corrigir(decimal valorInicial, IEnumerable<Indice> indices, DateTime dataInicial)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ValorCorrigido> CorrigirEDiscriminar(decimal valorInicial, IEnumerable<Indice> indices)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ValorCorrigido> CorrigirEDiscriminar(decimal valorInicial, IEnumerable<Indice> indices, DateTime dataInicial)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
